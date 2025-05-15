5e483d8eae016beadfdde38136e10f9d53cd7c7e
SantinoAlonso25 <salonso1@mail.austral.edu.ar>
2025-05-15 20:29:11 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -2,9 +2,8 @@
 
 def remove_elements(list_to_remove_elements):
     if len(list_to_remove_elements) >= 1:
-    	del list_to_remove_elements [0]
-    	del list_to_remove_elements [3:5]
-
+        del list_to_remove_elements[0]
+        del list_to_remove_elements[3:5]
     return list_to_remove_elements
 
 def add_elements(list_to_add_elements):
@@ -14,21 +13,21 @@ def add_elements(list_to_add_elements):
 
 def is_empty(list_to_check):
     if list_to_check == []:
-    	rta = True
+        rta = True
     else:
-    	rta = False
+        rta = False
     return rta
 
 def check_lists(list_to_compare1, list_to_compare2):
     if (len(list_to_compare1) >= 3) and (len(list_to_compare2) >= 3):
-    	if list_to_compare1[2] == list_to_compare2[2]
-    		rta = True
-    	else:
-    		rta = False
+        if list_to_compare1[2] == list_to_compare2[2]:
+            rta = True
+        else:
+            rta = False
     elif (len(list_to_compare1) < 3) or (len(list_to_compare2) < 3):
-    	rta = False
+        rta = False
     return rta
 
 def list_of_lists(list_of_lists_to_modify):
-    list_of_lists1 = [list_of_lists_to_modify [0] [:2], list_of_lists_to_modify [1] [1:4], list_of_lists_to_modify [2] [-2:]]
-    return list_of_lists1
+    lista_nueva = [list_of_lists_to_modify[0][:2], list_of_lists_to_modify[1][1:4], list_of_lists_to_modify[2][-2:]]
+    return lista_nueva
